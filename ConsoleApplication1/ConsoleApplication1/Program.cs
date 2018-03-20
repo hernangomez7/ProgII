278a220a4be35af56fbe0d63c3d51a04749a0de0
hernangomez7 <dath_math@hotmail.com>
2018-03-19 22:15:59 -0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int NumberRattle = 45;
+            bool entroFlag = false;
+            List<int>nombre= new List<int>();
+            //Console.Write("{0},{1}+{0}", 4, NumberRattle);
+            nombre.Add(1);
+            nombre.Add(2);
+            nombre.Add(3);
+            nombre.Add(4);
+
+           
+            /*for (int i=0; i < nombre.Count; i++) {
+                nombre.Add(i);
+            }
+            Console.Write("{0}", auxNombre);*/
+            for (int i=0; i < nombre.Count; i++) {
+                Console.WriteLine("numero: {0}", nombre[i]);
+                if (entroFlag == false)
+                {
+                    nombre.Remove(nombre[0]);
+                    entroFlag = true;
+                } 
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ejerciciosGuia/ejerciciosGuia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciosGuia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroIngresado[5];
+            string stringIngresado;
+            //do(int.tryParse(palabra,out aux))
+            
+
+            Console.WriteLine("Escriba un numero ");
+            stringIngresado = Console.ReadLine();
+            numeroIngresado = int.Parse(stringIngresado);
+
+            //Console.WriteLine("{0} ", numeroIngresado);
+
+            Console.ReadKey();
+        }
+    }
+}
